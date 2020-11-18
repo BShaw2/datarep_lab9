7df003e230a6c071fbd24731728473a20ee38a0c
brettshaw789 <brettshaw789@gmail.com>
2020-11-18 20:18:55 +0000

    Lab completed

--- src/components/movieitem.js
+++ src/components/movieitem.js
@@ -7,11 +7,11 @@ export class MovieItem extends React.Component {
         return (
             <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
                 <Card border="dark" style={{ width: '18rem' }}>
-                    <Card.Header>{this.props.movie.Title}</Card.Header>
+                    <Card.Header>{this.props.movie.title}</Card.Header>
                     <Card.Body>
-                        <Card.Title><img src={this.props.movie.Poster} width="200" height="200"></img></Card.Title>
+                        <Card.Title><img src={this.props.movie.poster} width="200" height="200"></img></Card.Title>
                         <Card.Text>
-                            {this.props.movie.Year}
+                            {this.props.movie.year}
                         </Card.Text>
                     </Card.Body>
                 </Card>
